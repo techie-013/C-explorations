b906b5e003e7b98393fc612267c74743086afca4
techie-013 <sneha.singh.1390@gmail.com>
2025-07-10 18:25:09 +0530

    Adding Two Numbers

---
+++ Adding_Two_Numbers.c
@@ -0,0 +1,12 @@
+//Program to add two numbers(integers)
+#include <stdio.h>
+int main(){
+    int num1,num2;
+    printf("Enter the first number: ");
+    scanf("%d",&num1);
+    printf("Enter the second number: ");
+    scanf("%d",&num2);
+    int sum = num1 + num2;
+    printf("The sum of %d and %d is : %d",num1,num2,sum);
+    return 0;
+}
